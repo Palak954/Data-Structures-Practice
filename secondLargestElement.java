bc1043b39807e65e730cbabe6f1bdb0e7d887f88
Palak954 <palakgoel46321@gmail.com>
2023-12-30 17:14:16 +0530

    modified:   secondLargestElement.java

--- secondLargestElement.java
+++ secondLargestElement.java
@@ -1,21 +1,20 @@
 public class secondLargestElement {
-	public static int findSecondLargest(int n, int[] arr) {
-		int first = Integer.MIN_VALUE;
-		int second = -1;
-		for(int i=0;i<n;i++){
-			if(arr[i] > first){
-				second = first;
-				first = arr[i];
-			}
-			else{
-				if(arr[i] > second && arr[i] != first)
-				second = arr[i];
-			}
-		}
-		
-		if(second == Integer.MIN_VALUE)
-		return -1;
+    public static int findSecondLargest(int n, int[] arr) {
+        int first = Integer.MIN_VALUE;
+        int second = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > first) {
+                second = first;
+                first = arr[i];
+            } else {
+                if (arr[i] > second && arr[i] != first)
+                    second = arr[i];
+            }
+        }
 
-		return second;
-	}
+        if (second == Integer.MIN_VALUE)
+            return -1;
+
+        return second;
+    }
 }
